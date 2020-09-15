dc8cb3107e3a617fbb24aa5f9e581994cef57b0e
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-15 09:51:35 +0200

    Initial commit

---
+++ Day15.py
@@ -0,0 +1,12 @@
+# AOC19 day 15
+
+
+def load_data(f_name):
+    with open(f_name, "r") as f:
+        data_read = f.read()
+    return data_read
+
+
+def run():
+    data = load_data("Day15.txt")
+
---
+++ Day23.py
@@ -0,0 +1,12 @@
+# AOC19 day 23
+
+
+def load_data(f_name):
+    with open(f_name, "r") as f:
+        data_read = f.read()
+    return data_read
+
+
+def run():
+    data = load_data("Day23.txt")
+
