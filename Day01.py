587e028f5bcbfcf170b5d22dc7086b4637d30c9d
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-15 13:19:36 +0200

    day 1 part 1

--- Day01.py
+++ Day01.py
@@ -7,6 +7,18 @@ def load_data(f_name):
     return data_read
 
 
+def fuel_requirement(mass):
+    return (mass // 3) - 2
+
+
+def get_fuel_sum(masses):
+    fuel = 0
+    for mass in masses:
+        fuel += fuel_requirement(mass)
+    return fuel
+
+
 def run():
     data = load_data("Day01.txt")
-
+    masses = list(map(int, data.split("\n")))
+    print(f"The sum of the fuel requirements is {get_fuel_sum(masses)}")
