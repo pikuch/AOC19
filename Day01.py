506a6da57d946069b916e32a7ddb99b391b054d5
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-15 13:27:06 +0200

    day 1 done

--- Day01.py
+++ Day01.py
@@ -11,6 +11,16 @@ def fuel_requirement(mass):
     return (mass // 3) - 2
 
 
+def corrected_fuel_requirement(mass):
+    fuel = 0
+    while True:
+        additional_fuel = (mass // 3) - 2
+        if additional_fuel <= 0:
+            return fuel
+        fuel += additional_fuel
+        mass = additional_fuel
+
+
 def get_fuel_sum(masses):
     fuel = 0
     for mass in masses:
@@ -18,7 +28,15 @@ def get_fuel_sum(masses):
     return fuel
 
 
+def get_corrected_fuel_sum(masses):
+    fuel = 0
+    for mass in masses:
+        fuel += corrected_fuel_requirement(mass)
+    return fuel
+
+
 def run():
     data = load_data("Day01.txt")
     masses = list(map(int, data.split("\n")))
-    print(f"The sum of the fuel requirements is {get_fuel_sum(masses)}")
+    print(f"The sum of fuel requirements is {get_fuel_sum(masses)}")
+    print(f"The real sum of fuel requirements is {get_corrected_fuel_sum(masses)}")
