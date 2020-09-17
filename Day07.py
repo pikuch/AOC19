8156b914445d342a93e8724ce62e4394d64fffb9
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-17 17:34:32 +0200

    day 7 done

--- Day07.py
+++ Day07.py
@@ -27,7 +27,30 @@ def test_phase_settings(data):
     return max_output
 
 
+def test_phase_settings_with_feedback(data):
+    amplis = []
+    for i in range(5):
+        amplis.append(Intcode())
+    max_output = 0
+    for phases in permutations(range(5, 10)):
+        for i in range(5):
+            amplis[i].load(data)
+            amplis[i].inputs.append(phases[i])
+        signal = 0
+        i = 0
+        while amplis[-1].state != "halted":
+            amplis[i % 5].inputs.append(signal)
+            amplis[i % 5].run()
+            signal = amplis[i % 5].outputs.popleft()
+            i += 1
+        if signal > max_output:
+            max_output = signal
+    return max_output
+
+
 def run():
     data = load_data("Day07.txt")
     max_signal = test_phase_settings(data)
+    print(f"The maximum signal that can be produced is {max_signal}")
+    max_signal = test_phase_settings_with_feedback(data)
     print(f"The maximum signal that can be produced is {max_signal}")
