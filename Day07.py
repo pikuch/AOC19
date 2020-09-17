beed1e23fb808671fb309ebee367e32e650cd5cc
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-17 14:25:13 +0200

    day 7 part 1

--- Day07.py
+++ Day07.py
@@ -1,4 +1,6 @@
 # AOC19 day 07
+from intcode import Intcode
+from itertools import permutations
 
 
 def load_data(f_name):
@@ -7,6 +9,25 @@ def load_data(f_name):
     return data_read
 
 
+def test_phase_settings(data):
+    amplis = []
+    for i in range(5):
+        amplis.append(Intcode())
+    max_output = 0
+    for phases in permutations(range(5)):
+        signal = 0
+        for i in range(5):
+            amplis[i].load(data)
+            amplis[i].inputs.append(phases[i])
+            amplis[i].inputs.append(signal)
+            amplis[i].run()
+            signal = amplis[i].outputs.popleft()
+        if signal > max_output:
+            max_output = signal
+    return max_output
+
+
 def run():
     data = load_data("Day07.txt")
-
+    max_signal = test_phase_settings(data)
+    print(f"The maximum signal that can be produced is {max_signal}")
