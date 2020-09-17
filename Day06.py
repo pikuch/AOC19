21004a97ad3922209832537922653b1b89f1b782
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-17 12:14:10 +0200

    day 6 test passed

--- Day06.py
+++ Day06.py
@@ -1,4 +1,5 @@
 # AOC19 day 06
+from collections import defaultdict
 
 
 def load_data(f_name):
@@ -7,6 +8,20 @@ def load_data(f_name):
     return data_read
 
 
-def run():
-    data = load_data("Day06.txt")
+def parse_orbits(data):
+    orbits = defaultdict(lambda: [])
+    for line in data.split("\n"):
+        planets = line.split(")")
+        orbits[planets[0]].append(planets[1])
+    return orbits
 
+
+def count_total_orbits(current, level, orbits):
+    return level + sum(map(lambda a: count_total_orbits(a, level+1, orbits), orbits[current]))
+
+
+def run():
+    data = load_data("Day06test.txt")
+    orbits = parse_orbits(data)
+    total_orbits = count_total_orbits("COM", 0, orbits)
+    print(f"The total number of direct and indirect orbits is {total_orbits}")
