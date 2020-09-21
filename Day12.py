18a908aaeb0968373b47b03a713753b6b0bfda11
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-21 20:39:09 +0200

    Day 12 part 1 done

--- Day12.py
+++ Day12.py
@@ -1,4 +1,5 @@
 # AOC19 day 12
+import numpy as np
 
 
 def load_data(f_name):
@@ -7,6 +8,29 @@ def load_data(f_name):
     return data_read
 
 
+def parse_data(data):
+    state = np.zeros((2, 4, 3), dtype=np.int32)
+    lines = data.split("\n")
+    for i in range(len(lines)):
+        words = lines[i].translate(str.maketrans("<>,=xyz", "       ")).split()
+        state[0, i] = list(map(int, words))
+    return state
+
+
+def update(state):
+    for i in range(4):
+        state[1, i] += np.sum(np.sign(state[0, :] - state[0, i]), axis=0)
+    state[0] += state[1]
+
+
+def calculate_total_energy(state):
+    return np.sum(np.sum(np.abs(state[0, :]), axis=1) * np.sum(np.abs(state[1, :]), axis=1), axis=0)
+
+
 def run():
     data = load_data("Day12.txt")
-
+    state = parse_data(data)
+    print(state)
+    for i in range(1000):
+        update(state)
+    print(f"The total energy of the system after 1000 steps is {calculate_total_energy(state)}")
