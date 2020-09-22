742f8ca563f56c29cb99f6c0b3ac66fd7a42d991
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-22 19:35:19 +0200

    tests passed

--- Day16.py
+++ Day16.py
@@ -1,4 +1,5 @@
 # AOC19 day 16
+import numpy as np
 
 
 def load_data(f_name):
@@ -7,6 +8,21 @@ def load_data(f_name):
     return data_read
 
 
-def run():
-    data = load_data("Day16.txt")
+def get_coefficients(row, col):
+    pos = ((col + 1) // (row + 1)) % 4
+    return 1 - abs(pos - 1)
 
+
+def fft(code, n):
+    coeffs = np.fromfunction(lambda row, col: get_coefficients(row, col), (len(code), len(code)), dtype=np.int)
+    for _ in range(n):
+        code = np.abs(np.dot(coeffs, code)) % 10
+    return code
+
+
+def run():
+    data = load_data("Day16test4.txt")
+    code = list(map(int, list(data)))
+
+    code = fft(code, 100)
+    print("".join(map(str, code))[:8])
