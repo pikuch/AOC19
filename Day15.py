e506c89f7e56ab9ac40d404071671e10664597b4
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-22 17:34:58 +0200

    Day 15 part 1 done

--- Day15.py
+++ Day15.py
@@ -1,4 +1,5 @@
 # AOC19 day 15
+from collections import deque
 from repairDroid import RepairDroid
 
 
@@ -8,6 +9,23 @@ def load_data(f_name):
     return data_read
 
 
+def find_distance(corridors):
+    to_check = deque()
+    to_check.append((0, 0, 0))
+    corridors[(0, 0)] = 0
+    while len(to_check):
+        x, y, dist = to_check.popleft()
+        for xx, yy in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]:
+            if corridors[(xx, yy)] < 0:
+                if corridors[(xx, yy)] == -1:
+                    corridors[(xx, yy)] = dist + 1
+                    to_check.append((xx, yy, dist + 1))
+                elif corridors[(xx, yy)] == -10:
+                    return dist + 1
+
+    return -1  # no path
+
+
 def run():
     data = load_data("Day15.txt")
     droid = RepairDroid()
@@ -15,4 +33,5 @@ def run():
     corridors = {(0, 0): -1}
     droid.explore(corridors)
 
+    print(f"The repair droid can get to the oxygen system in {find_distance(corridors)} steps")
 
