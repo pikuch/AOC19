d2043fa4705b1a2308fb7511e85e753709dfa4a0
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-23 17:55:51 +0200

    faster, still slow

--- Day18.py
+++ Day18.py
@@ -27,11 +27,13 @@ def parse_maze(data):
                 maze[row, col] = 0
                 row0, col0 = row, col
             else:
-                maze[row, col] = 1
                 if tile.isupper():
+                    maze[row, col] = 1
                     doors[tile] = (row, col)
                 else:
+                    maze[row, col] = 2
                     keys[tile] = (row, col)
+                    keys[(row, col)] = tile
 
     return maze, keys, doors, row0, col0
 
@@ -81,7 +83,54 @@ def shortest_all_keys_path(maze, keys, doors, row, col):
     return shortest
 
 
+# check which keys are possible
+def possible_keys(maze, keys, doors, row, col):
+    options = []
+    visited = set()
+    visited.add((row, col))
+    to_check = deque()
+    to_check.append((row, col, 0))
+    while len(to_check):
+        r, c, dist = to_check.popleft()
+        for dr, dc in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+            rr, cc = r + dr, c + dc
+            if maze[rr, cc] == 1:
+                continue
+            elif maze[rr, cc] == 2:
+                options.append((keys[(rr, cc)], dist + 1))
+            elif (rr, cc) not in visited and maze[rr, cc] == 0:
+                visited.add((rr, cc))
+                to_check.append((rr, cc, dist + 1))
+    return options
+
+
+# find the shortest path to all the keys by considering possibilities first
+def shortest_path_fast(maze, keys, doors, row, col):
+    options = possible_keys(maze, keys, doors, row, col)
+    min_dist = None
+    for grab, new_dist in options:
+        new_maze = maze.copy()
+        new_maze[keys[grab]] = 0
+        if grab.upper() in doors:
+            new_maze[doors[grab.upper()]] = 0
+        new_keys = keys.copy()
+        del new_keys[keys[grab]]
+        del new_keys[grab]
+        if len(new_keys):
+            result = shortest_path_fast(new_maze, new_keys, doors, keys[grab][0], keys[grab][1])
+            if result is None:
+                continue
+            dist = new_dist + result
+            if min_dist is None:
+                min_dist = dist
+            elif dist < min_dist:
+                min_dist = dist
+        else:
+            return new_dist
+    return min_dist
+
+
 def run():
-    data = load_data("Day18test4.txt")
+    data = load_data("Day18test5.txt")
     maze, keys, doors, row, col = parse_maze(data)
-    print(f"The shortest path to collect all the keys is {shortest_all_keys_path(maze, keys, doors, row, col)} steps long")
+    print(f"The shortest path to collect all the keys is {shortest_path_fast(maze, keys, doors, row, col)} steps long")
