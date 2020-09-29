3379b845d00675d0069cefd2453f5fba6870cf27
pikuch <55052790+pikuch@users.noreply.github.com>
2020-09-29 11:29:37 +0200

    Day 20 done

--- Day20.py
+++ Day20.py
@@ -65,8 +65,13 @@ def parse_input(data):
     links = {}
     for name, pos in portals.items():
         if name != "AA" and name != "ZZ":
-            links[(pos[0][1])] = pos[1][0]
-            links[(pos[1][1])] = pos[0][0]
+            if pos[0][1][0] < 4 or pos[0][1][0] >= maze.shape[0] - 4 or\
+               pos[0][1][1] < 4 or pos[0][1][1] >= maze.shape[1] - 4:
+                side = -1
+            else:
+                side = 1
+            links[(pos[0][1])] = pos[1][0][0], pos[1][0][1], side
+            links[(pos[1][1])] = pos[0][0][0], pos[0][0][1], -side
     # start and stop are special cases
     maze[portals["AA"][0][1]] = 1
     maze[portals["ZZ"][0][1]] = 3
@@ -89,9 +94,75 @@ def find_shortest_path(maze, links, start, stop):
             else:
                 if (new_row, new_col) not in visited:
                     if maze[new_row, new_col] == 2:
-                        new_row, new_col = links[(new_row, new_col)]
+                        new_row, new_col, _ = links[(new_row, new_col)]
                     visited.add((new_row, new_col))
                     to_check.append((new_row, new_col, dist + 1))
+
+    return -1  # not found
+
+
+def find_shortest_path_recursive(maze, links, start, stop):
+    visited = set()
+    visited.add((start[0], start[1], 0))
+    to_check = deque()
+    to_check.append((start[0], start[1], 0, 0))
+    while len(to_check):
+        row, col, depth, dist = to_check.popleft()
+        print(f"\rchecking {dist}...", end="")
+        if (row, col) == stop and depth == 0:
+            print(" found exit!")
+            return dist
+        for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+            new_row, new_col = row+dr, col+dc
+            if maze[new_row, new_col] == 1:
+                continue
+            else:
+                if (new_row, new_col, depth) not in visited:
+                    if maze[new_row, new_col] == 2:
+                        new_row, new_col, change_depth = links[(new_row, new_col)]
+                        new_depth = depth + change_depth
+                        if new_depth < 0:
+                            continue
+                        visited.add((new_row, new_col, new_depth))
+                        to_check.append((new_row, new_col, new_depth, dist + 1))
+                    else:
+                        visited.add((new_row, new_col, depth))
+                        to_check.append((new_row, new_col, depth, dist + 1))
+
+    return -1  # not found
+
+
+def find_shortest_path_recursive_fast(maze, links, start, stop, max_depth):
+    deep_maze = np.zeros((max_depth, maze.shape[0], maze.shape[1]), dtype=np.uint8)
+    deep_maze += maze  # transfer data quickly
+
+    deep_maze[0, start[0], start[1]] = 1  # visited
+
+    to_check = deque()
+    to_check.append((start[0], start[1], 0, 0))
+    last_dist = 0
+    while len(to_check):
+        row, col, depth, dist = to_check.popleft()
+        if dist > last_dist:
+            print(f"\rchecking {dist}", end="")
+            last_dist = dist
+        if (row, col) == stop and depth == 0:
+            print(" found exit!")
+            return dist
+        for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+            new_row, new_col = row+dr, col+dc
+            if deep_maze[depth, new_row, new_col] == 1:
+                continue
+            if deep_maze[depth, new_row, new_col] == 2:
+                new_row, new_col, change_depth = links[(new_row, new_col)]
+                new_depth = depth + change_depth
+                if new_depth < 0 or new_depth >= max_depth:
+                    continue
+                deep_maze[new_depth, new_row, new_col] = 1
+                to_check.append((new_row, new_col, new_depth, dist + 1))
+            else:
+                deep_maze[depth, new_row, new_col] = 1
+                to_check.append((new_row, new_col, depth, dist + 1))
 
     return -1  # not found
 
@@ -101,3 +172,5 @@ def run():
     maze, links, start, stop = parse_input(data)
     shortest = find_shortest_path(maze, links, start, stop)
     print(f"The shortest path from AA to ZZ is {shortest}")
+    shortest_recursive = find_shortest_path_recursive_fast(maze, links, start, stop, 32)
+    print(f"The shortest recursive path from AA to ZZ is {shortest_recursive}")
