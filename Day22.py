08cb9f71a3559bf48290cfdef117ace08493c9d0
pikuch <55052790+pikuch@users.noreply.github.com>
2020-10-01 08:51:53 +0200

    input

--- Day22.py
+++ Day22.py
@@ -7,6 +7,23 @@ def load_data(f_name):
     return data_read
 
 
+def parse_operations(data):
+    ops = []
+    for line in data.split("\n"):
+        if line == "deal into new stack":
+            ops.append((-1, 0))
+        elif line[:19] == "deal with increment":
+            ops.append([2, int(line.split()[3])])
+        elif line[:3] == "cut":
+            ops.append([1, int(line.split()[1])])
+        else:
+            print(f"Unrecognized operation: {line}")
+            exit(1)
+    return ops
+
+
 def run():
-    data = load_data("Day22.txt")
+    data = load_data("Day22test.txt")
+    ops = parse_operations(data)
+    deck = list(range(10))
 
